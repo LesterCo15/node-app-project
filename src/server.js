034fbe13146e4201eb61b2443e15033b3f91aa8d
John Lester Co <johnlesterco1596@gmail.com>
2025-12-26 16:09:33 +0800

    added Jenkinsfile

--- src/server.js
+++ src/server.js
@@ -9,7 +9,11 @@ app.get("/version", (req, res) => {
     res.json({ version: "1.0.0" });
 })
 
+app.get("/", (req, res) => {
+    res.send("Node API is running");
+})
+
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port $(port)`);
+    console.log(`App running on port ${port}`);
 })
